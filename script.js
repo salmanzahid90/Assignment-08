9248b3ac23372087214f47f1a11a54021d3e4ea4
Salman Zahid <122826565+salmanzahid90@users.noreply.github.com>
2023-03-01 00:15:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+// first problem
+var admin = "Umair Azmat"
+var admin = "Umair Azmat"
+alert(admin);
+
+
+// 2nd problem
+var planet = "earth";
+console.log(planet);
+
+
+// 3rd problem
+var currentVisitor = "umair Azmat";
+console.log(currentVisitor)
+
+
+// 4th problem
+var favDrink = "coca cola";
+console.log(favDrink);
+console.log(typeof favNum);
+
+
+// 5th problem
+var favNum = 19;
+console.log(favNum);
+console.log(typeof favNum);
+
+// 6th problem
+function myFavs(){
+    var color = 'red';
+    var food = 'Biryani';
+    var movie = 'F9';
+    console.log('My favourite color' + color + ".");
+    console.log('My favourite food' + food + ".");
+    console.log('My favourite movie' + movie + ".");
+}
+myFavs();
+
+
+//  7th Problem   
+var person = [name = "umair" , age = 20 ];
+console.log(person);
+console.log(typeof person);
+
+
+// 8th problem
+var a = 20;
+var b = 30;
+var c = a+b;
+console.log(c);
+
+
+// 9th problem
+var a = 5;
+var b = 2;
+var c = a/b;
+console.log(c);
+
+// 10th problem
+var a = 50;
+var b = 2;
+var  c = a+b*5;
+console.log(c)
+
+// 11th problem
+var f = 50;
+var c = (f-32)*5/9;
+console.log(c)
+
+// 12th Problem
+var a = parseFloat(prompt("Enter Your first Number"));
+var b = parseFloat(prompt("Enter Your second Number"));
+var c = a+b;
+alert(c);
+
+// 13th problem
+var a = prompt("Enter Your number");
+var b = prompt("Enter your number");
+if(a>b){
+    alert("A is greater then b");
+}else{
+    alert("A is smaller then b");
+}
+
+
+// 14th problem
+var a = parseFloat(prompt("Enter your first number"));
+var b = parseFloat(prompt("Enter your second number "));
+var c = a*b;
+alert(c)
+
+// 15th problem
+var a = parseFloat(prompt("Enter your first number "));
+var b = parseFloat(prompt("Enter your second number"));
+var c = a/b;
+alert(c);
+console.log(c);
+
+// 16th problem
+var a = parseFloat(prompt("Enter your number "));
+var b = parseFloata(prompt("Enter your number "));
+alert(c);
